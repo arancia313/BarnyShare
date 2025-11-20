cdd637391d671e1d611e7e9ca108965bbc9989b1
arancia313 <marraalan89@gmail.com>
2025-11-20 21:48:15 +0100

    I hope this works just like it is supposed to.

--- admin/index.mjs
+++ admin/index.mjs
@@ -8,8 +8,9 @@
         const Admin = getRandomAdmin();
             console.log("Verifying if you're an Admin or not.");
                           if (Admin === "2") {
-                             console.log("Creating commands...");
-            const Commands = [
+    if (prompt("Admin passcode?") === "78237382764864738") {
+        console.log("Creating commands...");
+                         const Commands = [
             ";guestBuster", ";shutdown",
             ";showGlobal", ";guest",
             ";unshowGlobal", ";strangerMode"
@@ -22,7 +23,19 @@
             BarnyWarp.commands = Commands;
         }
     } else {
+        console.log("WHOOPS! You've got the passcode wrong...")
+    }
+
+    } else {
         console.log("Unable to make commands: You're not an Admin.");
     }
 })(typeof BarnyWarp !== 'undefined' ? BarnyWarp : {});
-// This will run as soon as you're on BarnyWarp's Homepage.
+/*
+      This will run as soon as you're on BarnyWarp's Homepage.
+      It will basically decide if you're an Admin or not.
+      If the Admin thing returns 1, then you're not an Admin.
+      But if it returns 2, then YOU have to type a secret
+      Admin passcode. If you get it wrong, i'm sorry, but
+      you will not be an Admin. The passcode is complex,
+      you know.
+*/
