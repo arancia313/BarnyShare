f6c2338c74be47c0d2f6b2c321a7325940016bcf
arancia313 <marraalan89@gmail.com>
2025-11-20 19:35:09 +0100

    It needs to be good.

--- admin/index.mjs
+++ admin/index.mjs
@@ -1,23 +1,23 @@
-    // A script to BarnyWarp's Commands.
-
-             (function(BarnyWarp) {
+        (function(BarnyWarp) {
              // Creates the Commands if you're an Admin.
-                const Activated = ["1","2"]
-                   const Admin = Activated[Math.floor(Math.random() * Activated.Length)];
+                const Activated = ["1","2"];
+                   const Admin = Activated[Math.floor(Math.random() * Activated.length)];
                    console.log("Verifying if you're an Admin or not.");
-                          if (Admin === 2) {
+                          if (Admin === "2") {
                              console.log("Creating commands...");
-                                  const Commands = [
+            const Commands = [
             ";guestBuster", ";shutdown",
             ";showGlobal", ";guest",
             ";unshowGlobal", ";strangerMode"
             
         ];
-        console.log("You have access to BarnyWarp's Commands.")
+        alert("You have access to BarnyWarp's Commands.");
+        fetch("./admin/commands.json") // "fetch"? That's good.
         if (typeof BarnyWarp === 'object' && BarnyWarp !== null) {
             BarnyWarp.commands = Commands;
         }
     } else {
-        console.error("Unable to make commands: You're not an Admin.");
+        console.log("Unable to make commands: You're not an Admin.");
     }
 })(typeof BarnyWarp !== 'undefined' ? BarnyWarp : {});
+// This will run as soon as you're on BarnyWarp's Homepage.
