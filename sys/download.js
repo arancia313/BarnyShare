7d1a0fd9714587f4460db71fe4e235a2114169b7
arancia313 <marraalan89@gmail.com>
2025-11-29 18:53:35 +0100

    I removed a piece this big.

---
+++ sys/download.js
@@ -0,0 +1,14 @@
+        // Download button
+        $('#downloadBtn').click(function() {
+            if (confirm('Do you want to download BarnyWarp?')) {
+                if (prompt('Type "Yes" to continue.' = "Yes")) {
+                    alert('The webpage is installing the .zip folder for BarnyWarp.');
+                    window.location.href="https://github.com/arancia313/BarnyWarp/releases/download/get2/BarnyWarp.2.5.zip";
+                } else {
+                    alert("Download was cancelled.")
+                    console.warn("Fatal Error!")
+                };
+            } else {
+                window.location.href="https://arancia313.github.io/BarnyShare";
+            };
+        });
