160640f5a9b3f64930399646ccb2cb87404fbf7a
arancia313 <marraalan89@gmail.com>
2025-12-02 21:58:36 +0100

    You can now talk with extflashy. I inserted a script about that.

---
+++ devposts/13-11-25-extflashy/extflashy-quest.js
@@ -0,0 +1,14 @@
+(function(BarnyWarp) {
+     $.documentQuerySelector("#talk").onclick(function() {
+        // Daily quests.
+           if (prompt("What can i do for you?") = "???") {
+            alert(
+                 "%cNo assets were returned. This error is temporary and should not be reported.",
+                 "color:red"
+            );
+           };        
+        });
+     }); /*
+It's basically a system so that you can talk with
+Extflashy, BarnyWarp's AI.
+*/
