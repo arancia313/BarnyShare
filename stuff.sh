fb11e3d7b3651fed3252672f2249b4b2ce1d06d0
Arancia 3 Technologies <marraalan89@gmail.com>
2026-09-03 10:52:43 +0200

    Added this.

---
+++ stuff.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+clear
+echo "====================================================================="
+echo "hint: I'm begging you. Please do not use BarnyShare's code for jokes."
+echo "hint: It's extremely unecessary, stupid, and unhealthy."
+echo "====================================================================="
